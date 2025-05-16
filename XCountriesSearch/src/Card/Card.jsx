c958200e40f7758942524f409cfe694bc16759fc
SNEHARIITB <panabakasnehar@gmail.com>
2025-05-16 22:01:24 +0530

    test02

--- XCountriesSearch/src/Card/Card.jsx
+++ XCountriesSearch/src/Card/Card.jsx
@@ -8,7 +8,20 @@ export default function Card({ country }) {
   
     return (
       <>
-        <div className={styles.countryCard}>
+        <div className="countryCard"
+        style={{
+              height: "200px",
+              width: "200px",
+              border: "1px solid grey",
+              borderRadius: "5px",
+              
+              gap: "5px",
+              display: "flex",
+              flexDirection:" column",
+              textAlign: "center",
+              justifyContent: "center",
+              alignItems: "center",
+        }}>
           <img src={country.png} alt={country.common} className={styles.imageflag} />
           <h2 className={styles.cname}>{country.common}</h2>
         </div>
